371f2b4d8cf35ccd77dffaf09de67037db93a3f1
turtledev <turtledev65@gmail.com>
2025-01-28 22:08:37 +0200

    Format the files

--- src/Csv/parser.cpp
+++ src/Csv/parser.cpp
@@ -2,110 +2,110 @@
 
 namespace Csv
 {
-    Parser::Parser(std::istream &input): m_Input(&input)
-    {
-        if (!m_Input->good()) {
-            throw std::runtime_error("Something is wrong with the input stream");
-        }
-        m_FieldBuffer.reserve(FIELD_BUFFER_CAPACITY);
+Parser::Parser(std::istream &input) : m_Input(&input)
+{
+  if (!m_Input->good()) {
+    throw std::runtime_error("Something is wrong with the input stream");
+  }
+  m_FieldBuffer.reserve(FIELD_BUFFER_CAPACITY);
 
-        // Read the input stream
-        m_Input->read(m_InputBuffer.data(), INPUT_BUFFER_CAPACITY);
-        m_BytesRead = static_cast<size_t>(m_Input->gcount());
-        m_Eof = m_Input->eof();
+  // Read the input stream
+  m_Input->read(m_InputBuffer.data(), INPUT_BUFFER_CAPACITY);
+  m_BytesRead = static_cast<size_t>(m_Input->gcount());
+  m_Eof       = m_Input->eof();
+}
+
+Field Parser::nextField()
+{
+  m_FieldBuffer.clear();
+
+  for (;;) {
+    char *token = topToken();
+    if (token == nullptr) {
+      m_State = State::END_OF_CSV;
+      return m_FieldBuffer.empty() ? Field(FieldType::CSV_END)
+                                   : Field(std::move(m_FieldBuffer));
     }
 
-    Field Parser::nextField()
-    {
-        m_FieldBuffer.clear();
+    char ch = *token;
+    switch (m_State) {
+    case State::START_OF_FIELD:
+      if (ch == m_NewLine) {
+        m_State = State::END_OF_ROW;
+      }
 
-        for (;;) {
-            char *token = topToken();
-            if (token == nullptr) {
-                m_State = State::END_OF_CSV;
-                return m_FieldBuffer.empty() ? Field(FieldType::CSV_END) 
-                                             : Field(std::move(m_FieldBuffer));
-            }
+      if (ch == m_Quote) {
+        m_State = State::IN_QUOTED_FIELD;
+      } else if (ch == m_Delimiter) {
+        return Field(std::move(m_FieldBuffer));
+      } else {
+        m_State = State::IN_FIELD;
+        m_FieldBuffer += ch;
+      }
 
-            char ch = *token;
-            switch (m_State) {
-                case State::START_OF_FIELD:
-                    if (ch == m_NewLine) {
-                        m_State = State::END_OF_ROW;
-                    }                    
+      m_Cursor++;
+      break;
 
-                    if (ch == m_Quote) {
-                        m_State = State::IN_QUOTED_FIELD;
-                    } else if (ch == m_Delimiter) {
-                        return Field(std::move(m_FieldBuffer));
-                    } else {
-                        m_State = State::IN_FIELD;
-                        m_FieldBuffer += ch;
-                    }
+    case State::IN_FIELD:
+      if (ch == m_NewLine) {
+        m_State = State::END_OF_ROW;
+        return Field(std::move(m_FieldBuffer));
+      }
+      if (ch == m_Delimiter) {
+        m_State = State::START_OF_FIELD;
+        return Field(std::move(m_FieldBuffer));
+      }
 
-                    m_Cursor ++;
-                    break;
-                
-                case State::IN_FIELD:
-                    if (ch == m_NewLine) {
-                        m_State = State::END_OF_ROW;
-                        return Field(std::move(m_FieldBuffer));
-                    }
-                    if (ch == m_Delimiter) {
-                        m_State = State::START_OF_FIELD;
-                        return Field(std::move(m_FieldBuffer));
-                    }
-                
-                    m_FieldBuffer += ch;
-                    m_Cursor ++;
-                    break;
-                
-                case State::IN_QUOTED_FIELD:
-                    if (ch == m_Quote) {
-                        m_State = State::IN_ESCAPED_QUOTE;
-                    } else {
-                        m_FieldBuffer += ch;
-                    }
-                
-                    m_Cursor ++;
-                    break;
-                
-                case State::IN_ESCAPED_QUOTE:
-                    if (ch == m_NewLine) {
-                        m_State = State::END_OF_ROW;
-                        return Field(std::move(m_FieldBuffer));
-                    }
-                
-                    if (ch == m_Quote) {
-                        m_State = State::IN_QUOTED_FIELD;
-                        m_FieldBuffer += ch;
-                    } else if (ch == m_Delimiter) {
-                        m_State = State::START_OF_FIELD;
-                        return Field(std::move(m_FieldBuffer));
-                    } else {
-                        m_State = State::IN_FIELD;
-                        m_FieldBuffer += ch;
-                    }
+      m_FieldBuffer += ch;
+      m_Cursor++;
+      break;
 
-                    m_Cursor ++;
-                    break;
-                
-                case State::END_OF_ROW:
-                    m_State = State::START_OF_FIELD;
-                    return Field(FieldType::ROW_END);
-                
-                case State::END_OF_CSV:
-                    throw std::runtime_error("Something went wrong");
-            }
-        }
+    case State::IN_QUOTED_FIELD:
+      if (ch == m_Quote) {
+        m_State = State::IN_ESCAPED_QUOTE;
+      } else {
+        m_FieldBuffer += ch;
+      }
+
+      m_Cursor++;
+      break;
+
+    case State::IN_ESCAPED_QUOTE:
+      if (ch == m_NewLine) {
+        m_State = State::END_OF_ROW;
+        return Field(std::move(m_FieldBuffer));
+      }
+
+      if (ch == m_Quote) {
+        m_State = State::IN_QUOTED_FIELD;
+        m_FieldBuffer += ch;
+      } else if (ch == m_Delimiter) {
+        m_State = State::START_OF_FIELD;
+        return Field(std::move(m_FieldBuffer));
+      } else {
+        m_State = State::IN_FIELD;
+        m_FieldBuffer += ch;
+      }
+
+      m_Cursor++;
+      break;
+
+    case State::END_OF_ROW:
+      m_State = State::START_OF_FIELD;
+      return Field(FieldType::ROW_END);
+
+    case State::END_OF_CSV:
+      throw std::runtime_error("Something went wrong");
     }
+  }
+}
 
-    char *Parser::topToken()
-    {
-        if (m_Cursor == m_BytesRead && m_Eof) {
-            return nullptr;
-        }
+char *Parser::topToken()
+{
+  if (m_Cursor == m_BytesRead && m_Eof) {
+    return nullptr;
+  }
 
-        return &m_InputBuffer[m_Cursor];
-    }
+  return &m_InputBuffer[m_Cursor];
+}
 } // namespace Csv
--- src/Csv/parser.hpp
+++ src/Csv/parser.hpp
@@ -1,67 +1,64 @@
 #pragma once
 
 #include <iostream>
-#include <fstream>
 #include <vector>
 
 namespace Csv
 {
-    enum class FieldType
-    {
-        DATA,
-        ROW_END,
-        CSV_END,
-    };
-    struct Field
-    {
-        explicit Field(FieldType t): type(t) {}
-        explicit Field(const std::string &&str)
-            : type(FieldType::DATA), data(std::move(str)) {}
-        
-        FieldType type;
-        std::string data;
-    };
+enum class FieldType {
+  DATA,
+  ROW_END,
+  CSV_END,
+};
+struct Field {
+  explicit Field(FieldType t) : type(t)
+  {
+  }
+  explicit Field(const std::string &&str)
+      : type(FieldType::DATA), data(std::move(str))
+  {
+  }
 
-    class Parser
-    {
-        public:
-            Parser(std::istream &input);
+  FieldType   type;
+  std::string data;
+};
 
-            Field nextField();
+class Parser
+{
+public:
+  Parser(std::istream &input);
 
-           
-        private:
-            // Configurable attributes
-            std::istream *m_Input;
-            char m_Delimiter = ';';
-            char m_Quote = '"';
-            char m_NewLine = '\n';
+  Field nextField();
 
+private:
+  // Configurable attributes
+  std::istream *m_Input;
+  char          m_Delimiter = ';';
+  char          m_Quote     = '"';
+  char          m_NewLine   = '\n';
 
-            // Buffers
-            static constexpr int FIELD_BUFFER_CAPACITY = 1024;
-            static constexpr int INPUT_BUFFER_CAPACITY = 1024 * 128;
-            std::string m_FieldBuffer {};
-            std::vector<char> m_InputBuffer = std::vector<char>(INPUT_BUFFER_CAPACITY); 
+  // Buffers
+  static constexpr int FIELD_BUFFER_CAPACITY = 1024;
+  static constexpr int INPUT_BUFFER_CAPACITY = 1024 * 128;
+  std::string          m_FieldBuffer{};
+  std::vector<char>    m_InputBuffer = std::vector<char>(INPUT_BUFFER_CAPACITY);
 
-            // Used for reading the buffers
-            std::size_t m_Cursor = 0;
-            std::size_t m_BytesRead = 0;
-            bool m_Eof = false;
+  // Used for reading the buffers
+  std::size_t m_Cursor    = 0;
+  std::size_t m_BytesRead = 0;
+  bool        m_Eof       = false;
 
-            // State of the cursor
-            enum class State 
-            {
-                START_OF_FIELD,
-                IN_FIELD,
-                IN_QUOTED_FIELD,
-                IN_ESCAPED_QUOTE,
-                END_OF_ROW,
-                END_OF_CSV,
-            };
-            State m_State = State::START_OF_FIELD;
+  // State of the cursor
+  enum class State {
+    START_OF_FIELD,
+    IN_FIELD,
+    IN_QUOTED_FIELD,
+    IN_ESCAPED_QUOTE,
+    END_OF_ROW,
+    END_OF_CSV,
+  };
+  State m_State = State::START_OF_FIELD;
 
-            char *topToken();
-
-    };
+  char *topToken();
+};
 } // namespace Csv
