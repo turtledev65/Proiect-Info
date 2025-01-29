2f054ad064b00f60b2cf541cbb584da2caeb41d2
turtledev <turtledev65@gmail.com>
2025-01-29 18:23:26 +0200

    Add a more complicated csv file and parse it

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,31 @@
-#include <iostream>
 #include <fstream>
 #include "Csv/parser.hpp"
 
+struct Area {
+  Area(const std::string &str) : name(str)
+  {
+  }
+
+  std::string name = "";
+};
+
 int main()
 {
-  std::ifstream file("res/tes.csv");
-  Csv::Parser parser(file);
-  Csv::Field field = parser.nextField();
-  std::cout << field.data << std::endl;
+  std::ifstream file("res/Recensamant-2021.csv");
+  Csv::Parser   parser =
+      Csv::Parser(file).delimiter(',').quote('"').newLine('\n');
+
+  std::vector<Area> areas;
+  for (auto &row : parser) {
+    for (auto &field : row) {
+      bool isArea = row.size() == 2 && field.size() > 0;
+      if (isArea) {
+        areas.emplace_back(std::move(field));
+      }
+    }
+  }
+  std::cout << areas.size() << std::endl;
+  for (auto &area : areas) {
+    std::cout << area.name << std::endl;
+  }
 }
