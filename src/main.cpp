0fbf4c84ac4e0256f800753de3b3166587cb136f
turtledev <turtledev65@gmail.com>
2025-02-09 14:24:21 +0200

    Use the cli library in the main file

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,143 @@
 #include "Html/generator.hpp"
 #include "Statistics/area.hpp"
 #include "Core/utils.hpp"
+#include "Cli/app.hpp"
 
 #include <fstream>
 
 using namespace std;
 using namespace Statistics;
+using namespace Utils;
+namespace fs = std::filesystem;
 
-// Functions
-vector<Area> readCsvFile(const string &path);
-string       convertAreasToJson(const vector<Area> &areas);
+// Subprograme
+std::vector<fs::path> gasesteFisiereleCSV();
+vector<Area>          citesteFisierCSV(const fs::path &fisier);
+void                  listeazaFisiere();
+vector<Area>          selecteazaFisier(size_t id);
+void                  genereazaDocumentHTML(const std::vector<Area> &zone,
+                                            const fs::path          &sablon,
+                                            const fs::path          &fisierGenerat);
 
 // Main
 int main()
 {
-  // Read data from csv file
-  vector<Area> areas = readCsvFile("res/data/Recensamant-2021.csv");
+  std::vector<Area> zone;
 
-  // Generate the html document
-  Html::Generator generator("res/statistics-website-template/index.html");
-  generator.define("AREAS", Html::ConvertType::JSON,
-                   [areas]() { return convertAreasToJson(areas); });
-  generator.parseTemplateToFile("./index.html");
+  Cli::App app("Recensamant Romania");
+  app.AddHelpCommand("ajutor", "Vezi o lista cu toate comenzile si ce fac");
+  app.AddExitCommand("iesi", "Inchide programul");
+  app.AddCommand(
+      "ls", [](const vector<string> &args) { listeazaFisiere(); },
+      "Listeaza toate fisierele CSV gasite, care contin statistici");
+  app.AddCommand(
+      "sel",
+      [&zone](const vector<string> &args) {
+        if (args.empty()) {
+          cout << "ID-ul trebuie specificat";
+          return;
+        }
+
+        size_t id;
+        try {
+          id   = stoi(args[0]);
+          zone = selecteazaFisier(id);
+          if (zone.empty()) {
+            cout << "Nu s-a putu citi fisierul\n";
+          } else {
+            cout << "Fisierul a fost citit cu succes.\n";
+          }
+        } catch (const std::invalid_argument &) {
+          cout << "ID invalid: " << id << '\n';
+        } catch (const std::out_of_range &) {
+          cout << "ID-ul este prea mare: " << args[0] << "\n";
+        } catch (...) {
+          cout << "Eroare necunoscuta la procesarea ID-ului\n";
+        }
+      },
+      "Selecteaza un fisier cu statistici folosind ID-ul acestuia");
+  app.AddCommand(
+      "gen",
+      [&zone](const vector<string> &args) {
+        if (zone.empty()) {
+          cout << "Niciun fisier selectat\n";
+          return;
+        }
+        genereazaDocumentHTML(zone,
+                              "./res/statistics-website-template/index.html",
+                              "./statistici.html");
+      },
+      "Genereaza documentul HTLM pentru fisierul cu statistici selectat");
+
+  app.Start();
+
+  return 0;
 }
 
-// Function Implementations
-string convertAreasToJson(const vector<Area> &areas)
+// Implementarea subprogramelor
+std::vector<fs::path> gasesteFisiereleCSV()
 {
-  stringstream out;
-
-  out << "`{";
-  for (size_t i = 0; i < areas.size(); i++) {
-    const Area &area = areas[i];
-    out << area.toJSON();
-    if (i < areas.size() - 1) {
-      out << ',';
-    }
+  fs::path rootPath = fs::current_path().append("res").append("data");
+  if (!fs::exists(rootPath)) {
+    cout << "Could not find data folder\n";
   }
-  out << "}`";
 
-  return out.str();
+  vector<fs::path> files = getDirectoryFiles(rootPath, {".csv"});
+  return files;
 }
 
-vector<Area> readCsvFile(const string &path)
+void listeazaFisiere()
 {
-  ifstream file(path);
+  vector<fs::path> files = gasesteFisiereleCSV();
+  for (size_t i = 0; i < files.size(); i++) {
+    cout << '[' << i << ']' << ' ' << files[i].filename().string() << '\n';
+  }
+}
+
+vector<Area> selecteazaFisier(size_t id)
+{
+  vector<fs::path> files = gasesteFisiereleCSV();
+  if (id >= files.size()) {
+    cout << id << " este un ID invalid.\n";
+    return {};
+  }
+
+  return citesteFisierCSV(files[id]);
+}
+
+void genereazaDocumentHTML(const std::vector<Area> &zone,
+                           const fs::path          &sablon,
+                           const fs::path          &fisierGenerat)
+{
+  try {
+    Html::Generator generator(sablon);
+    generator.define("AREAS", Html::ConvertType::JSON, [zone]() {
+      stringstream out;
+
+      out << "`{";
+      for (size_t i = 0; i < zone.size(); i++) {
+        const Area &zona = zone[i];
+        out << zona.toJSON();
+        if (i < zone.size() - 1) {
+          out << ',';
+        }
+      }
+      out << "}`";
+
+      return out.str();
+    });
+    generator.parseTemplateToFile(fisierGenerat);
+
+    std::cout << "Fisier HTML generat in: \n"
+              << fs::absolute(fisierGenerat).string() << '\n';
+  } catch (const std::exception &e) {
+    std::cout << "Fisierul HTML nu a putut fi generate\n" << '\t' << e.what();
+  }
+}
+
+vector<Area> citesteFisierCSV(const fs::path &fisier)
+{
+  ifstream file(fisier);
   if (!file.is_open()) {
     throw runtime_error("File not found");
   }
